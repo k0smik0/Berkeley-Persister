c91381c1c34aa29a22e64d70794c670e9804eda8
Massimiliano Leone <maximilianus@gmail.com>
2015-02-02 02:50:37 +0100

    added LGPLv3 license

--- src/net/iubris/berkeley_persister/dao/base/DAO.java
+++ src/net/iubris/berkeley_persister/dao/base/DAO.java
@@ -1,19 +1,15 @@
 /*******************************************************************************
- * Copyleft (c) 2014, "Massimiliano Leone - <maximilianus@gmail.com> - https://plus.google.com/+MassimilianoLeone"
- * This file (PersisterDAO.java) is part of Persister.
+ * Copyleft (c) 2015, "Massimiliano Leone - <maximilianus@gmail.com> - https://plus.google.com/+MassimilianoLeone"
+ * This file (DAO.java) is part of BerkeleyPersister.
  * 
- *     PersisterDAO.java is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ *     DAO.java is free software: you can redistribute it and/or modify
+ *     it under the terms of the Lesser GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * 
- *     PersisterDAO.java is distributed in the hope that it will be useful,
+ *     DAO.java is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See   the Lesser GNU General Public License for more details.
  * 
- *     You should have received a copy of the GNU General Public License
- *     along with .  If not, see <http://www.gnu.org/licenses/>.
+ *     You should have received a copy of the Lesser GNU General Public License along with .  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
 package net.iubris.berkeley_persister.dao.base;
 
